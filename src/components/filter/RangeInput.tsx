6146a0416fa75c86cfae98c34a5cdef0845bc46a
Mahedul-Karim <karimrupo21@gmail.com>
2023-11-23 09:21:32 +0600

    initial commit

--- src/components/filter/RangeInput.tsx
+++ src/components/filter/RangeInput.tsx
@@ -1,59 +1,70 @@
+//@ts-nocheck
 "use client";
+
 import React, { useState, useRef, useEffect } from "react";
 
 const RangeInput = () => {
-  const [minValue, setMinValue] = useState(280);
+  const [minValue, setMinValue] = useState(2800);
   const [maxValue, setMaxValue] = useState(6500);
   const rangeRef: any = useRef();
+  const inputRef: any = useRef([]);
 
-  const rangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if ((maxValue - minValue) < 1000) {
-      if (e.target.classList.contains("min__value")) {
-        setMinValue(maxValue - 1000);
-      }else{
-        setMaxValue(minValue + 1000);
-      }
-      
-    } else {
-      if (e.target.classList.contains("min__value")) {
-        setMinValue(+e.target.value);
+  const pushRef = (el) => inputRef.current.push(el);
 
-        rangeRef.current.style.left =
-          (+e.target.value / +e.target.max) * 100 + "%";
-      }
-      if (e.target.classList.contains("max__value")) {
-        setMaxValue(+e.target.value);
-        rangeRef.current.style.right =
-          100 - (+e.target.value / +e.target.max) * 100 + "%";
-      }
-    }
-  };
+  useEffect(() => {
+    inputRef.current.forEach((input) => {
+      input.addEventListener("input", (e) => {
+        let minVal = parseInt(inputRef.current[0].value);
+
+        let maxVal = parseInt(inputRef.current[1].value);
+        if (maxVal - minVal < 1000) {
+          if (e.target.classList.contains("range-min")) {
+            inputRef.current[0].value = maxVal - 1000;
+          } else {
+            inputRef.current[1].value = minVal + 1000;
+          }
+        } else {
+          setMinValue(inputRef.current[0].value);
+
+          setMaxValue(inputRef.current[1].value);
+
+          rangeRef.current.style.left =
+            (minVal / inputRef.current[0].max) * 100 + "%";
+
+          rangeRef.current.style.right =
+            100 - (maxVal / inputRef.current[1].max) * 100 + "%";
+        }
+      });
+    });
+  }, []);
 
   return (
     <div>
       <h3 className="text-[20px] font-[400] mb-4">Pricing scale</h3>
       <div className="relative w-full h-[10px] bg-border rounded-[15px]">
         <div
-          className="absolute left-[28%] right-[37%] h-[10px] bg-primary"
+          className="absolute left-[28%] right-[37%] h-[10px] bg-primary rounded-[15px]"
           ref={rangeRef}
         ></div>
       </div>
       <div className="relative">
         <input
           type="range"
-          className="min__value"
+          className="min__value range__input range-min"
           min="0"
           max="10000"
-          value={minValue}
-          onChange={(e) => rangeValue(e)}
+          defaultValue={"2800"}
+          step="100"
+          ref={pushRef}
         />
         <input
           type="range"
-          className="max__value"
+          className="max__value range__input"
           min="0"
           max="10000"
-          value={maxValue}
-          onChange={(e) => rangeValue(e)}
+          defaultValue={"6500"}
+          step="100"
+          ref={pushRef}
         />
       </div>
     </div>
--- src/components/tour/Sidebar.tsx
+++ src/components/tour/Sidebar.tsx
@@ -3,10 +3,11 @@ import Filter from "../filter/Filter";
 
 const Sidebar = () => {
   return (
-    <div className="bg-white rounded-2xl shadow-lg px-4 py-4 hidden lg:flex flex-col gap-4 h-max pb-[40px]">
+    <div className="bg-white rounded-2xl shadow-lg px-4 py-4 flex flex-col gap-4 h-max pb-[40px]">
       <Filter />
     </div>
   );
 };
 
 export default Sidebar;
+//bg-white rounded-2xl shadow-lg px-4 py-4 hidden lg:flex flex-col gap-4 h-max pb-[40px]
