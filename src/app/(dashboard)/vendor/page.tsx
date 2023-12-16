9348f85ab9763f4012fff0516cadaf9869474282
Mahedul-Karim <karimrupo21@gmail.com>
2023-12-16 19:26:40 +0600

    prisma bug fix

--- src/app/(dashboard)/vendor/page.tsx
+++ src/app/(dashboard)/vendor/page.tsx
@@ -1,6 +1,11 @@
-import Vendor from "@/components/dashboard/vendor/main/vendor-dashboard/Vendor";
 import Sidebar from "@/components/dashboard/vendor/side/Sidebar";
 import React from "react";
+import dynamic from "next/dynamic";
+
+const Vendor = dynamic(
+  () => import("@/components/dashboard/vendor/main/vendor-dashboard/Vendor"),
+  { ssr: false }
+);
 
 const VendorPage = () => {
   return (
